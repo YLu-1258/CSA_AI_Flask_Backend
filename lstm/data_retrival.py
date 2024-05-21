fc99d649ec6e2d418de92a6454ca82e51524d60a
YLu-1258 <maodou1258@gmail.com>
2024-05-21 09:44:17 -0700

    Commit
    Co-authored-by: dolphinalt <dolphinalt@users.noreply.github.com>

--- lstm/data_retrival.py
+++ lstm/data_retrival.py
@@ -1,24 +1,31 @@
 import yfinance as yf
 import pandas as pd
 import datetime
+from dateutil.relativedelta import relativedelta
+from pathlib import Path
 
-tickers = ["GOOGL", "AMZN", "AAPL", "TSLA", "WMT", "MSFT", "META", "COST", "LMT", "NOC", "UNH"]
+def generate_data():
+    tickers = ["GOOGL", "AMZN", "AAPL", "TSLA", "WMT", "MSFT", "META", "COST", "LMT", "NOC", "UNH"]
 
-current_date = datetime.date.today()
+    current_date = datetime.date.today()
+    start_date = current_date - relativedelta(years=10)
 
-for ticker in tickers:
-    print("Processing ticker:", ticker)
-    data = yf.download(ticker, start="2004-01-01", end=current_date)
-    
-    # Reset index to make 'Date' a column
-    data.reset_index(inplace=True)
-    
-    # Add 'Ticker' column with ticker value
-    data['Symbol'] = ticker
-    
-    # Reorder the columns
-    data = data[['Date', 'Symbol', 'Open', 'Close', 'Low', 'High', 'Volume']]
-    
-    # Save to CSV
-    file_name = f"./data/{ticker}.csv"
-    data.to_csv(path_or_buf=file_name, index=False)
+    for ticker in tickers:
+        print("Processing ticker:", ticker)
+        data = yf.download(ticker, start=start_date, end=current_date)
+        
+        # Reset index to make 'Date' a column
+        data.reset_index(inplace=True)
+        
+        # Add 'Ticker' column with ticker value
+        data['Symbol'] = ticker
+        
+        # Reorder the columns
+        data = data[['Date', 'Symbol', 'Open', 'Close', 'Low', 'High', 'Volume']]
+        
+        # Save to CSV
+        current_directory = Path(__file__).parent.resolve()
+        file_name = f"{current_directory}/data/{ticker}.csv"
+        data.to_csv(path_or_buf=file_name, index=False)
+
+generate_data()
